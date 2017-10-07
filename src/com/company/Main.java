36e812918885887b788b41d22c6c1a0012119f8e
Yisandy Marte <YisandyMarte@ac.c4q.nyc>
2017-10-07 17:57:22 -0400

    homework2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,36 +6,50 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int question = 0;
+        Scanner keyboard = new Scanner(System.in);
 
-        String[] questions = {"you're swimming in the Pacific Ocean, what do you do?", "you get eaten by a shark and die the most painful excrutiating way"};
-        String[] answers ={"keep swimming", "end game"};
+        String Go, Look, Pantry, Eat;
 
-        System.out.println("Welcome to my game! Please answer the questions...");
+        System.out.println("WELCOME TO MY TINY ADVENTURE");
+        System.out.println("  ");
+        System.out.println("You are in a creepy house! Would you like to go 'upstairs' or into the 'kitchen'? ");
+        System.out.print("> ");
+        Go = keyboard.next();
 
-        while(true) {
+        if (Go.equalsIgnoreCase("kitchen")) {
+            System.out.println("There is a long counter top with dirty dishes everywhere. Off to one side there is, as you'd expect, a refrigerator. You may open the 'refrigerator' or look in the 'pantry'. ");
+            System.out.print(">  ");
+            Look = keyboard.next();
 
-            System.out.println(questions[question]);
+            if (Look.equalsIgnoreCase("refrigerator")) {
+                System.out.println("Inside the refrigerator you see food and stuff. It looks pretty nasty. Would you like to eat some of the food, 'Yes' or 'No'?");
+                System.out.print(">  ");
+                Eat = keyboard.next();
 
-            System.out.println("Answer:  ");
-            String answer = new Scanner(System.in).nextLine();
+                if (Eat.equalsIgnoreCase("Yes")) {
+                    System.out.println("  ");
+                    System.out.println("You live!");
+                } else if (Eat.equalsIgnoreCase("No")) {
+                    System.out.println("  ");
+                    System.out.println("You die of starvation!");
+                }
+            } else if (Look.equalsIgnoreCase("pantry")) {
+                System.out.println("There is a killer inside. Do you want to 'fight' them, or 'run away'?");
+                System.out.print(">  ");
+                Pantry = keyboard.next();
 
-            if(answer.equals(answers[question])){
-                question ++;
-
-                if(question == questions.length)
-                    break;
-
-                System.out.println("That's obvious!");
+                if (Pantry.equalsIgnoreCase("fight")) {
+                    System.out.println("  ");
+                    System.out.println("You're weak and die");
+                } else if (Pantry.equalsIgnoreCase("run away")) {
+                    System.out.println("  ");
+                    System.out.println("You died because your too slow & can't run");
+                }
 
             }
-            else{
-                question = 0;
-
-                System.out.println("You would never want to do something like that, I'll give you another try.");
+            else {
+                System.out.println("Wrong move you are now dead. Til next time.");
             }
-            System.out.println("The end.");
         }
-
     }
 }
